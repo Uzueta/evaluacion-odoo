2609b8d1a0a2f6b1d2bae6d1cfe242a1005d4565
Uzueta <uzueta.m@hotmail.com>
2023-10-11 21:40:15 -0700

    Se relaciona modulo Estate con otros, se agreganc campos Many2one, Fin de capitulo 8

--- custom_addons/estate/__manifest__.py
+++ custom_addons/estate/__manifest__.py
@@ -2,6 +2,7 @@
     'name': "Estate",
     'depends': ['base'],
     'data': [
+        'views/estate_property_offer_views.xml',
         'views/estate_property_tag_views.xml',
         'views/estate_property_type_views.xml',
         'views/estate_property_views.xml',
